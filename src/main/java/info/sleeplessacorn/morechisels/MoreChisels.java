c8b242e79668423df04d8d2ad7cd9830707794eb
InsomniaKitten <admin@designdevils.co.uk>
2017-06-29 21:49:17 +0100

    Base class and Chisel dependency

---
+++ src/main/java/info/sleeplessacorn/morechisels/MoreChisels.java
@@ -0,0 +1,46 @@
+package info.sleeplessacorn.morechisels;
+
+/*
+ *  Copyright 2017 Sleepless Acorn
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+import info.sleeplessacorn.morechisels.lib.LibInfo;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(
+        modid = LibInfo.MOD_ID,
+        name = LibInfo.MOD_NAME,
+        version = LibInfo.MOD_VERSION,
+        dependencies = "required-after:chisel@[" + LibInfo.CHISEL_VERSION + ",);",
+        acceptedMinecraftVersions = LibInfo.MC_VERSION
+)
+public class MoreChisels {
+
+    @Mod.Instance
+    public static MoreChisels instance;
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public void onInit(FMLInitializationEvent event) {}
+
+    @Mod.EventHandler
+    public void onPostInit(FMLPostInitializationEvent event) {}
+
+}
---
+++ src/main/java/info/sleeplessacorn/morechisels/lib/LibInfo.java
@@ -0,0 +1,29 @@
+package info.sleeplessacorn.morechisels.lib;
+
+/*
+ *  Copyright 2017 Sleepless Acorn
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+public class LibInfo {
+
+    public static final String MOD_ID = "morechisels";
+    public static final String MOD_NAME = "More Chisels";
+
+    public static final String MOD_VERSION = "%mod_version%";
+    public static final String MC_VERSION = "%mc_version%";
+
+    public static final String CHISEL_VERSION = "%chisel_version";
+
+}
